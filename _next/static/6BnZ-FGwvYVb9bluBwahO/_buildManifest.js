d40db80822165e057422e8656c9bb4382a1e3a2a
Burhan <apple@Apples-MacBook-Pro.local>
2026-03-13 17:14:33 +0500

    Updates

---
+++ _next/static/6BnZ-FGwvYVb9bluBwahO/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a071c2ea850473fa.js"
+  ],
+  "/_error": [
+    "static/chunks/e0c3a8fd776177c9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
