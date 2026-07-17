5c09aceb1af7bd97367448e739356e7409034c8f
Burhan <apple@Apples-MacBook-Pro.local>
2026-07-17 23:51:44 +0500

    Updates

--- _next/static/chunks/048d65fc2b3b70cb.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/9e0aa8132ee17443.js",
-  "static/chunks/034229dc6ed4da2b.js",
-  "static/chunks/turbopack-85b474db732cb539.js"
-])
--- _next/static/chunks/509dc43ca9093035.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/a14d625cf3fb18a9.js",
-  "static/chunks/289b6bf4bd08a8da.js",
-  "static/chunks/034229dc6ed4da2b.js",
-  "static/chunks/turbopack-0cc6d326263af59b.js"
-])
